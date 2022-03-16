00edc32d14b3ff5834f82bbc6ab246dd9e5dadea
amirmahdi askartajik <mahditajik428@gmail.com>
2022-03-16 16:51:47 +0330

    finished the sections no completing the examples

--- src/Enums.ts
+++ src/Enums.ts
@@ -1,22 +1,2 @@
-// Enums are one of the few features TypeScript has which is not a type-level extension of JavaScript.
-
-// Enums allow a developer to define a set of named constants. Using enums can make it easier to document intent,
-// or create a set of distinct cases. TypeScript provides both numeric and string-based enums.
-
-// this sectio is mostly copy pasted from the TypeScript handbook
+// you can the hanbook for this too
 // https://www.typescriptlang.org/docs/handbook/enums.html
-
-// Numeric enums
-
-// We’ll first start off with numeric enums,
-// which are probably more familiar if you’re coming from other languages.
-// An enum can be defined using the enum keyword.
-
-
-
-//-----------------------------------------------
-
-// ES2015/ES6/ECMAscript2015/etc version
-
-// note: es2015 code will be compiled to older versions of ecmascript
-// you can change this behavior with the "--target es6" flag
